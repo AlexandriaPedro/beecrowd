a6b3307330da0d85dc2493fb66833440d43541ef
alexandria <alexandriapedroo@gmail.com>
2025-08-30 23:18:57 -0300

    gitignore + java questions (1003 + 1004)

--- beginner/1004-simple-product/Main.java
+++ beginner/1004-simple-product/Main.java
@@ -1,10 +1,21 @@
 import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Main {
- 
     public static void main(String[] args) throws IOException {
-        System.out.print("Hello World!\n");
- 
+        Scanner scanner = new Scanner(System.in);
+
+        int a;
+        int b;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+
+        int prod;
+
+        prod = a * b;
+
+        System.out.println("PROD = " + prod);
     }
- 
 }

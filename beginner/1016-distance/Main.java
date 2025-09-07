4980aca932fcaacfa75ad2264892a39cf94fdcce
alexandria <alexandriapedroo@gmail.com>
2025-09-07 12:08:10 -0300

    bee 1015 and 1016 done (java + c)

---
+++ beginner/1016-distance/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int distance = scanner.nextInt();
+
+        int minutes = distance * 2;
+
+        System.out.println(minutes + " minutos");
+    }
+}
---
+++ beginner/1016-distance/distanceBetweenTwoPoints.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int distance;
+
+    scanf("%d", &distance);
+
+    int minutes = distance * 2;
+
+    printf("%d minutos\n", minutes);
+
+    return 0;
+}
